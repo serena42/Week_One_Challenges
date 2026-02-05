fdde63f898ef9a84a1859786e09a59de3670cb05
serena <serena@volpe>
2026-02-05 13:39:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week_One_Challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
